56e0bf59fd52a77dc7a45d0cccb39a3e44e4f7ae
wasita mahaphanit <wasita@brown.edu>
2020-04-28 14:03:42 -0400

    feat: bare bones version -- working

--- server/src/app.py
+++ server/src/app.py
@@ -1,20 +1,20 @@
 import os
 from flask import Flask, render_template, make_response, Blueprint
-import rlwmpst
+# import rlwmpst
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/get_rlwmpst_trials')
-def get_rlwmpst_trials():
-    '''get rlwmpst task logic to generate timeline'''
-    return rlwmpst.get_trials()
-
-
+@app.route('/')
 def index():
     '''flask by default will look for index.html'''
     return render_template('index.html')
+
+
+# @app.route('/get_rlwmpst_trials')
+# def get_rlwmpst_trials():
+#     '''get rlwmpst task logic to generate timeline'''
+#     return rlwmpst.get_trials()
 
 
 if __name__ == '__main__':
