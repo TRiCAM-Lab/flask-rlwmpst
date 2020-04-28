efeef377813f6d3422c5effb51a445e36c3a43a4
wasita mahaphanit <wasita@brown.edu>
2020-04-28 13:11:05 -0400

    feat: try to incorporate rlwmpst import call into app.py

--- server/src/app.py
+++ server/src/app.py
@@ -4,23 +4,19 @@ import rlwmpst
 
 app = Flask(__name__)
 
-# explore the Blueprint
-custom_code = Blueprint('custom_code', __name__,
-                        template_folder='templates', static_folder='./static')
 
-
-@custom_code.route('/', methods=['GET', 'POST'])
-@custom_code.route('/get_rlwmpst_trials')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/get_rlwmpst_trials')
 def get_rlwmpst_trials():
     '''get rlwmpst task logic to generate timeline'''
     return rlwmpst.get_trials()
 
 
-@app.route('/')
 def index():
     '''flask by default will look for index.html'''
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0',
+            port=int(os.environ.get('PORT', 8080)))
