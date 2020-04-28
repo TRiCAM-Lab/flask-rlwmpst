79b45817f855dc13058fa193c91f79f7693fe4c5
wasita mahaphanit <wasita@brown.edu>
2020-04-28 14:28:04 -0400

    feat: add firebase serve command to bash script

--- server/build-n-deploy.sh
+++ server/build-n-deploy.sh
@@ -5,3 +5,6 @@ gcloud builds submit --tag gcr.io/rlwmpst/flask-rlwmpst
 
 echo "Deploy to Firebase"
 gcloud run deploy flask-rlwmpst --region us-central1 --platform managed --image gcr.io/rlwmpst/flask-rlwmpst
+
+echo "Serving on Firebase localhost:5000"
+firebase serve
