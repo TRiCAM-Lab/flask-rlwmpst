ab368d9be40c3bac0f6a45d9feb0126525d47bc4
wasita mahaphanit <wasita@brown.edu>
2020-05-07 12:48:45 -0400

    debug: removed custom route; try py file ajax again

--- server/src/app.py
+++ server/src/app.py
@@ -1,13 +1,21 @@
 import os
 from flask import Flask, render_template, make_response, Blueprint
+import rlwmpst
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     '''flask by default will look for index.html'''
     return render_template('index.html')
+
+
+# @app.route('/get_rlwmpst_trials')
+# def get_rlwmpst_trials():
+#     print('do i get here')
+#     return rlwmpst.get_trials()
 
 
 if __name__ == '__main__':
