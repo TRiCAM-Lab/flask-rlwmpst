c6c9b8368754a3f99972ad578a358d0c9a2b0de9
wasita mahaphanit <wasita@brown.edu>
2020-05-26 12:56:45 -0400

    feat: qualtrics iframe attempt

--- server/build-n-deploy.sh
+++ server/build-n-deploy.sh
@@ -14,3 +14,9 @@ firebase serve
 
 # to test docker image locally:
 # PORT=8080 && docker run -p 9090:${PORT} -e PORT=${PORT} gcr.io/rlwmpst/flask-rlwmpst
+
+# RELEVANT LINKS:
+# https://rlwmpst.web.app/
+# http://localhost:5000/
+# https://cntracs.herokuapp.com/
+# https://cloud.google.com/run/docs/troubleshooting
